f19c7489e1a680c74bb4d9c15026ba1d42124ad0
quachphuong272 <hannychan123@gmail.com>
2020-11-04 15:36:51 +0700

    Initial commit / create git

---
+++ app/src/main/java/com/example/appsmartlearn/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.appsmartlearn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+    }
+
+}
